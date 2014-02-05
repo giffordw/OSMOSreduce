9827ee705dfdacc7c356d671ff96bc9011411cd2
giffordw <dgifford19@gmail.com>
2014-02-05 10:12:35 -0500

    Quad term added to wavelength calibration

--- testopt.py
+++ testopt.py
@@ -37,24 +37,24 @@ def air_to_vacuum(airwl,nouvconv=True):
         newwl[:] *= convfact
     return newwl[0] if isscal else newwl
 
-def wavecalibrate(px,fx,stretch_est=None,shift_est=None,parnum=2):
-    def prob1(x,x_p,F_p,w_m,F_m,st_es,sh_es):
-        interp = interp1d(x_p*x[0]+x[1],F_p,bounds_error=False,fill_value=0)
+def wavecalibrate(px,fx,stretch_est=None,shift_est=None,quad_est=None,parnum=2):
+    def prob1(x,x_p,F_p,w_m,F_m,st_es,sh_es,qu_es):
+        interp = interp1d(qu_es*x_p**2+x_p*x[0]+x[1],F_p,bounds_error=False,fill_value=0)
         #interp = interpolate.splrep(x_p*x[0]+x[1],F_p,s=0)
-        if np.abs(sh_es - x[1]) > 15: P0 = -np.inf
+        if np.abs(sh_es - x[1]) > 10: P0 = -np.inf
         else: P0 = 0.0
-        if np.abs(st_es - x[0]) > 0.015: P1 = -np.inf
+        if np.abs(st_es - x[0]) > 0.010: P1 = -np.inf
         else: P1 = 0.0
         return np.sum(F_m[np.where((w_m>4000)&(w_m<5300))]+interp(w_m[np.where((w_m>4000)&(w_m<5300))])) + P0 + P1
         #return np.sum(interp(w_m[np.where((w_m>4300)&(w_m<5300))])/F_m[np.where((w_m>4300)&(w_m<5300))])
         #return np.sum(F_m[np.where((w_m>4300)&(w_m<5300))]+interpolate.splev(w_m[np.where((w_m>4300)&(w_m<5300))],interp,der=0))
         #return spearmanr(F_m[np.where((w_m>4300)&(w_m<5300))],interp(w_m[np.where((w_m>4300)&(w_m<5300))]))[0] + P0 + P1 #+ np.sum(F_m[np.where((w_m>4300)&(w_m<5300))]/np.max(F_m[np.where((w_m>4300)&(w_m<5300))])+interp(w_m[np.where((w_m>4300)&(w_m<5300))])/np.max(interp(w_m[np.where((w_m>4300)&(w_m<5300))])))
-    def prob2(x,x_p,F_p,w_m,F_m,st,sh_es):
-        interp = interp1d(x[0]*xp**2+x_p*x[1]+x[2],F_p,bounds_error=False,fill_value=0)
+    def prob2(x,x_p,F_p,w_m,F_m,st_es,sh_es,qu_es):
+        interp = interp1d(x[0]*x_p**2+x_p*x[1]+x[2],F_p,bounds_error=False,fill_value=0)
         #interp = interpolate.splrep(x_p*x[0]+x[1],F_p,s=0)
-        if np.abs(sh_es - x[1]) > 5: P0 = -np.inf
+        if np.abs(sh_es - x[1]) > 10: P0 = -np.inf
         else: P0 = 0.0
-        if np.abs(st_es - x[0]) > 0.005: P1 = -np.inf
+        if np.abs(st_es - x[0]) > 0.010: P1 = -np.inf
         else: P1 = 0.0
         return np.sum(F_m[np.where((w_m>4000)&(w_m<5300))]+interp(w_m[np.where((w_m>4000)&(w_m<5300))])) + P0 + P1
 
@@ -67,17 +67,19 @@ def wavecalibrate(px,fx,stretch_est=None,shift_est=None,parnum=2):
         if stretch_est is not None:
             stretch_0 = stretch_est
             shift_0 = shift_est
+            quad_0 = quad_est
         else:
-            stretch_0 = 0.72
+            stretch_0 = 0.68
             shift_0 = 0.0
+            quad_0 = 1e-5
         print stretch_0,shift_0
-        stretch_est,shift_est = interactive_plot(px,fx,wm,fm,stretch_0,shift_0)
+        stretch_est,shift_est,quad_est = interactive_plot(px,fx,wm,fm,stretch_0,shift_0,quad_0)
     
     if parnum == 2:
         print 'Running linear fit'
         ndim,nwalkers = 2,10
         p0 = np.vstack((np.random.uniform(stretch_est-0.04,stretch_est+0.04,nwalkers),np.random.uniform(-10,10,nwalkers)+shift_est)).T
-        sampler = emcee.EnsembleSampler(nwalkers,ndim,prob1,args=[px,fx,wm,fm,stretch_est,shift_est])
+        sampler = emcee.EnsembleSampler(nwalkers,ndim,prob1,args=[px,fx,wm,fm,stretch_est,shift_est,quad_est])
         print 'Stepping MCMC'
         pos, prob, state = sampler.run_mcmc(p0,500)
         sampler.reset()
@@ -93,8 +95,9 @@ def wavecalibrate(px,fx,stretch_est=None,shift_est=None,parnum=2):
     if parnum == 3:
         print 'Running quadratic fit'
         ndim,nwalkers = 3,10
-        p0 = np.vstack((np.random.uniform(0.0,0.001,nwalkers),np.random.uniform(stretch_est-0.04,stretch_est+0.04,nwalkers),np.random.uniform(-10,10,nwalkers)+shift_est)).T
-        sampler = emcee.EnsembleSampler(nwalkers,ndim,prob2,args=[px,fx,wm,fm,stretch_est,shift_est])
+        p0 = np.vstack((np.random.uniform(1e-6,1e-5,nwalkers),np.random.uniform(stretch_est-0.005,stretch_est+0.005,nwalkers),np.random.uniform(-5,5,nwalkers)+shift_est)).T
+        print 'stretch_est',stretch_est
+        sampler = emcee.EnsembleSampler(nwalkers,ndim,prob2,args=[px,fx,wm,fm,stretch_est,shift_est,quad_est])
         print 'Stepping MCMC'
         pos, prob, state = sampler.run_mcmc(p0,1000)
         sampler.reset()
@@ -108,6 +111,8 @@ def wavecalibrate(px,fx,stretch_est=None,shift_est=None,parnum=2):
         (n_shift,bins_shift) = np.histogram(sampler.flatchain[:,2],100)
         midbins_shift = (bins_shift[:-1]+bins_shift[1:])/2.0
         max_shift = midbins_shift[np.where(n_shift==np.max(n_shift))]
+        plt.hist(sampler.flatchain[:,1],100)
+        plt.show()
         print 'Quad:',max_quad,'Shift:',max_shift,'Stretch:',max_stretch
     
     xs = np.linspace(shift_est - 100.0,shift_est + 100.0,1000)
@@ -128,29 +133,33 @@ def wavecalibrate(px,fx,stretch_est=None,shift_est=None,parnum=2):
     #plt.axvline(scimin['x'],color='g')
     #plt.show()
     #pdb.set_trace()
-    if parnum == 3:
-        if max_quad.size > 1: max_quad = max_quad[np.floor(max_quad.size/2.0)]
+    
+    #if parnum == 3:
+    #    if max_quad.size > 1: max_quad = max_quad[np.floor(max_quad.size/2.0)]
     if max_stretch.size > 1: max_stretch = max_stretch[np.floor(max_stretch.size/2.0)]
     if max_shift.size > 1: max_shift = max_shift[np.floor(max_shift.size/2.0)]
 
-    if parnum == 2:
-        return (px*max_stretch+max_shift,fx,max_stretch,max_shift)#(px*max_stretch+scimin['x'],fx,max_stretch,scimin['x'])
-    if parnum == 3:
-        return (max_quad*px**2 + px*max_stretch + max_shift,fx,max_stretch,max_shift)#(px*max_stretch+scimin['x'],fx,max_stretch,scimin['x'])
+    #if parnum == 2:
+    #    return (px*max_stretch+max_shift,fx,max_stretch,max_shift)#(px*max_stretch+scimin['x'],fx,max_stretch,scimin['x'])
+    #if parnum == 3:
+    return (quad_est*px**2 + px*max_stretch + max_shift,fx,quad_est,max_stretch,max_shift)#(px*max_stretch+scimin['x'],fx,max_stretch,scimin['x'])
 
-def interactive_plot(px,fx,wm,fm,stretch_0,shift_0):
+def interactive_plot(px,fx,wm,fm,stretch_0,shift_0,quad_0):
     fig,ax = plt.subplots()
     plt.subplots_adjust(left=0.25,bottom=0.30)
-    l, = plt.plot(stretch_0*px+shift_0,fx/10.0,'b')
+    l, = plt.plot(quad_0*px**2+stretch_0*px+shift_0,fx/10.0,'b')
     plt.plot(wm,fm/2.0,'ro')
     for i in range(wm.size): plt.axvline(wm[i],color='r')
-    plt.xlim(4200,5200)
+    plt.xlim(4000,6000)
     plt.ylim(0,3500)
 
+    
     axstretch = plt.axes([0.25,0.17,0.65,0.03])
     axshift = plt.axes([0.25,0.22,0.65,0.03])
+    fn_quad_0 = 0.0
     fn_stretch_0 = 0.0
     fn_shift_0 = 0.0
+    fn_axquad = plt.axes([0.25,0.03,0.65,0.03])
     fn_axstretch = plt.axes([0.25,0.07,0.65,0.03])
     fn_axshift = plt.axes([0.25,0.12,0.65,0.03])
     close_ax = plt.axes([0.05,0.5,0.13,0.1])
@@ -159,13 +168,14 @@ def interactive_plot(px,fx,wm,fm,stretch_0,shift_0):
     slide_shift = Slider(axshift,'Shift',-4000.0,4000.0,valinit=shift_0)
     fn_slide_stretch = Slider(fn_axstretch, 'Fine Stretch',-0.05,0.05,valinit=fn_stretch_0)
     fn_slide_shift = Slider(fn_axshift,'Fine Shift',-200.0,200.0,valinit=fn_shift_0)
+    fn_slide_quad = Slider(fn_axquad,'Fine Quad',-2e-5,2e-5,valinit=fn_quad_0)
     close_button = Button(close_ax,'Close Plots', hovercolor='0.80')
 
     def update(val):
-        l.set_xdata(slide_stretch.val*px+slide_shift.val)
+        l.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
         fig.canvas.draw_idle()
     def fineupdate(val):
-        l.set_xdata((slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l.set_xdata((quad_0+fn_slide_quad.val)*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
         #slide_stretch.val = slide_stretch.val + fn_slide_stretch.val
         #slide_shift.val = slide_shift.val + fn_slide_shift.val
         fig.canvas.draw_idle()
@@ -175,20 +185,22 @@ def interactive_plot(px,fx,wm,fm,stretch_0,shift_0):
     slide_shift.on_changed(update)
     fn_slide_stretch.on_changed(fineupdate)
     fn_slide_shift.on_changed(fineupdate)
+    fn_slide_quad.on_changed(fineupdate)
     close_button.on_clicked(close_plots)
     plt.show()
     shift_est = slide_shift.val+fn_slide_shift.val
     stretch_est = slide_stretch.val+fn_slide_stretch.val
-    return stretch_est,shift_est
+    quad_est = 1e-5 + fn_slide_quad.val
+    return stretch_est,shift_est,quad_est
 
-def interactive_plot_plus(px,fx,wm,fm,stretch_0,shift_0):
+def interactive_plot_plus(px,fx,wm,fm,stretch_0,shift_0,quad_0):
     #main plot
     fig,ax = plt.subplots()
     plt.subplots_adjust(left=0.25,bottom=0.30)
-    l, = plt.plot(stretch_0*px+shift_0,fx/10.0,'b')
+    l, = plt.plot(quad_0*px**2+stretch_0*px+shift_0,fx/10.0,'b')
     plt.plot(wm,fm/2.0,'ro')
     for i in range(wm.size): plt.axvline(wm[i],color='r')
-    plt.xlim(4200,5200)
+    plt.xlim(4000,6000)
     plt.ylim(0,3500)
 
     axstretch = plt.axes([0.25,0.17,0.65,0.03])
@@ -209,27 +221,27 @@ def interactive_plot_plus(px,fx,wm,fm,stretch_0,shift_0):
     s = plt.figure()
     ax2 = s.add_subplot(211)
     ax3 = s.add_subplot(212)
-    l2, = ax2.plot(stretch_0*px+shift_0,fx/10.0,'b')
+    l2, = ax2.plot(quad_0*px**2+stretch_0*px+shift_0,fx/10.0,'b')
     ax2.plot(wm,fm/2.0,'ro')
     for i in range(wm.size): ax2.axvline(wm[i],color='r')
     ax2.set_xlim(4490,4600)
     ax2.set_ylim(0,1000)
-    l3, = ax3.plot(stretch_0*px+shift_0,fx/10.0,'b')
+    l3, = ax3.plot(quad_0*px**2+stretch_0*px+shift_0,fx/10.0,'b')
     ax3.plot(wm,fm/2.0,'ro')
     for i in range(wm.size): ax3.axvline(wm[i],color='r')
     ax3.set_xlim(4900,5100)
     ax3.set_ylim(0,1500)
 
     def update(val):
-        l.set_xdata(slide_stretch.val*px+slide_shift.val)
-        l2.set_xdata(slide_stretch.val*px+slide_shift.val)
-        l3.set_xdata(slide_stretch.val*px+slide_shift.val)
+        l.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l2.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l3.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
         fig.canvas.draw_idle()
         s.canvas.draw_idle()
     def fineupdate(val):
-        l.set_xdata((slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
-        l2.set_xdata((slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
-        l3.set_xdata((slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l2.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
+        l3.set_xdata(quad_0*px**2+(slide_stretch.val+fn_slide_stretch.val)*px+(slide_shift.val+fn_slide_shift.val))
         #slide_stretch.val = slide_stretch.val + fn_slide_stretch.val
         #slide_shift.val = slide_shift.val + fn_slide_shift.val
         fig.canvas.draw_idle()
@@ -245,7 +257,7 @@ def interactive_plot_plus(px,fx,wm,fm,stretch_0,shift_0):
     plt.show()
     shift_est = slide_shift.val+fn_slide_shift.val
     stretch_est = slide_stretch.val+fn_slide_stretch.val
-    return (px*stretch_est+shift_est,fx,stretch_est,shift_est)
+    return (quad_0*px**2+px*stretch_est+shift_est,fx,stretch_est,shift_est)
 
 if __name__ == '__main__':
     from astropy.io import fits as pyfits
