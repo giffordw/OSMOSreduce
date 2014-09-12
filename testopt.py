87b0c9e2e52b8eace4ba5cbc99aba2e14ad1b2a7
giffordw <dgifford19@gmail.com>
2014-09-12 11:01:42 -0400

    changed backend to Qt4 for keypress ability on OSX with Anaconda

--- testopt.py
+++ testopt.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from matplotlib.widgets import Slider, Button, RadioButtons
